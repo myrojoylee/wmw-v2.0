50b20107b216e29f8f6050425e3e02e4a94ba519
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-11-10 01:26:10 -0500

    added build script to root package

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import useCurrentWeather from "./hooks/useCurrentWeather";
 
 const App = (): JSX.Element => {
