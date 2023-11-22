3ef7a0c81411cba4bf24ad0e8010b3c0ec8a5575
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-11-22 00:04:24 -0500

    added more styling and created map box

--- client/src/components/Weather.tsx
+++ client/src/components/Weather.tsx
@@ -24,8 +24,8 @@ const Weather = ({ currentWeather }: Props): JSX.Element => {
   return (
     <>
       {currentWeather ? (
-        <section className="flex flex-col w-full m-10 p-2 bg-white">
-          <div className="flex items-center justify-between">
+        <section className="flex flex-col w-full m-10 p-2 bg-white gap-5">
+          <div className="flex flex-col md: flex-row items-center md:justify-between">
             <p className="text-2xl font-light">
               <span className="uppercase font-bold">
                 {currentWeather?.name}
@@ -37,27 +37,36 @@ const Weather = ({ currentWeather }: Props): JSX.Element => {
               {formatDate(currentWeather.dt, currentWeather.timezone)}
             </p>
           </div>
-          <div>
-            <p>Current Weather</p>
-            <p>
-              Local time is{" "}
-              {formatTime(currentWeather.dt, currentWeather.timezone)}
-            </p>
-            <div className="flex items-center text-4xl">
-              <p>{Math.round(currentWeather.main.temp)} &deg;F</p>
-              {currentWeather.weather[0] ? (
-                <>
-                  <img
-                    src={`https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}.png`}
-                  />
-                </>
-              ) : null}
+          <div className="flex w-full flex-col md:flex-row lg:flex-row gap-5">
+            <div className="flex flex-col w-1/2">
+              <div className="flex flex-col gap-4">
+                <div>
+                  <p>Current Weather</p>
+                  <p>
+                    Local time is{" "}
+                    {formatTime(currentWeather.dt, currentWeather.timezone)}
+                  </p>
+                </div>
+                <div className="flex items-center text-4xl">
+                  <p>{Math.round(currentWeather.main.temp)} &deg;F</p>
+                  {currentWeather.weather[0] ? (
+                    <>
+                      <img
+                        src={`https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}.png`}
+                      />
+                    </>
+                  ) : null}
+                </div>
+              </div>
+              <div className="weather-today-stats">
+                <p>Description: {currentWeather.weather[0].description}</p>
+                <p>Wind: {currentWeather.wind.speed} MPH</p>
+                <p>Humidity: {currentWeather.main.humidity} %</p>
+              </div>
             </div>
-          </div>
-          <div className="weather-today-stats">
-            <p>{currentWeather.weather[0].description}</p>
-            <p>Wind: {currentWeather.wind.speed} MPH</p>
-            <p>Humidity: {currentWeather.main.humidity} %</p>
+            <div className="flex w-full md:w-1/2 border-2 border-black p-2 rounded-md">
+              Map goes here
+            </div>
           </div>
         </section>
       ) : (
